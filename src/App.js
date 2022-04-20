3cc059e789273f3f935f83f2ae474e1bf052b4b3
Armatova <armatovaaizusha@gmail.com>
2022-04-20 21:21:55 +0600

    first

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+// import ReactDOM from "react-dom";
+import "./App.css";
+import MovieContextProvider from "./context/contextsMovie";
+import Routing from "./Routing";
+import "antd/dist/antd.css";
+import CartContextProvider from "./context/cartContext";
+import FavoriteContextProvider from "./context/favoritesContext";
+// import AuthContextProvider from "./context/authContext";
+import ChatContextProvider from "./context/chatContext";
+
+function App() {
+  return (
+    <>
+      <ChatContextProvider>
+        {/* <AuthContextProvider> */}
+        <MovieContextProvider>
+          <FavoriteContextProvider>
+            <CartContextProvider>
+              <Routing />
+            </CartContextProvider>
+          </FavoriteContextProvider>
+        </MovieContextProvider>
+        {/* </AuthContextProvider> */}
+      </ChatContextProvider>
+    </>
+  );
+}
+
+export default App;
